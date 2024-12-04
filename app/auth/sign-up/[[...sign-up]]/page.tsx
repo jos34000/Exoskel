dabfc22661168ebca16b3a796b80519dc6d7b68b
jos34000 <jocelynsainson@icloud.com>
2024-12-04 17:20:46 +0100

    Update dependencies, integrate semantic release tools, and refactor authentication pages. Added new semantic release packages for automated versioning and changelog generation. Enhanced sign-in and sign-up pages with improved layout for better user experience. Removed unused sidebar items and streamlined sidebar component structure.

--- app/auth/sign-up/[[...sign-up]]/page.tsx
+++ app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
-  return <SignUp />;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-screen">
+      <SignUp />
+    </div>
+  );
 }
